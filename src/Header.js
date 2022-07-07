12e82b53e821dbdc571473e0a62111434775a98a
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-07-07 02:19:21 +0100

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import './Header.css';
-
+import Logo from './images/amazon.png';
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
     <div className='header'>
-        <img className="header__logo" src={require('./amazon.png').default} alt='amazon logo' />
+        <img className="header__logo" src={Logo} alt='amazon logo' />
         <div className="header__search">
             <input className="header__searchInput" type="text" />
+            <SearchIcon className="header__searchIcon"/>
             {/* Logo */}
         </div>
         <div className="header__nav">
             <div className='header__option'>
-                <span className='header__optionLineOnline'>Hello Guest</span>
+                <span className='header__optionLineOne'>Hello Guest</span>
                 <span className='header__optionLineTwo'>Sign In</span>
             </div>
             <div className='header__option'>
-                <span className='header__optionLineOnline'>Returns</span>
+                <span className='header__optionLineOne'>Returns</span>
                 <span className='header__optionLineTwo'>Orders</span>
             </div>
             <div className='header__option'>
-            <span className='header__optionLineOnline'>Your</span>
+            <span className='header__optionLineOne'>Your</span>
                 <span className='header__optionLineTwo'>Prime</span>
             </div>
+            <div className="header__optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header__optionLineTwo header__basketCount">0</span>
+            </div>
+
            
         </div>
     </div>
